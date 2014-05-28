e8d6755c9430349ab525e63fb3c15596b58c1ce7
Warren Smith <sharpsw@gmail.com>
2014-05-28 14:50:25 -0400

    Reimplemented fifth and third problems.

--- Employers/Twitter/Practice/Fifth Problem/PracticeProblem.java
+++ Employers/Twitter/Practice/Fifth Problem/PracticeProblem.java
@@ -1,31 +1,26 @@
-// Write a method to validate that a tree is a BST
-// If we perform an in order traversal on the tree all the elements should increases
+// Implement a function to check if a tree is a valid BST
 public class PracticeProblem {
 
     public static void main(String[] args) {
-
         Node node = new Node(20);
         node.left = new Node(10);
-        node.right = new Node(30);
-        node.right.left = new Node(18);
-        node.right.left.left = new Node(1);
-        node.right.left.right = new Node(5000);
-        node.left.left = new Node(1);
-        node.left.right = new Node(15);
+        node.left.left = new Node(5);
+        node.left.right = new Node(12);
 
-        System.out.println("Is valid BST: " + isValidBST(node, 0, 100000));
+        node.right = new Node(30);
+        node.right.left = new Node(25);
+        node.right.right = new Node(40);
+
+        System.out.println("Is a valid BST: " + validBST(node, 0, 100));
     }
 
-    public static boolean isValidBST(Node node, int min, int max) {
+    public static boolean validBST(Node node, int min, int max) {
+
         if (node == null) return true;
 
         if (node.value < min || node.value > max) return false;
 
-        if (!isValidBST(node.left, min, node.value)
-                || !isValidBST(node.right, node.value, max)) {
-            return false;
-        }
-        return true;
+        return validBST(node.left, min, node.value) && validBST(node.right, node.value, max);
     }
 
 }
--- Employers/Twitter/Practice/Third Problem/PracticeQuestion.java
+++ Employers/Twitter/Practice/Third Problem/PracticeQuestion.java
@@ -1,37 +1,35 @@
 import java.lang.Math;
 
+// Determine if a binary search tree is balanced
 public class PracticeQuestion {
 
     public static void main(String[] args) {
         Node node = new Node(20);
-        node.left = new Node(10);
-        node.right = new Node(30);
-        node.left.left = new Node(9);
-        node.left.right = new Node(15);
+        //node.left = new Node(10);
+        //node.left.left = new Node(5);
+        //node.left.right = new Node(12);
 
-        System.out.println("Tree balanced: " + isBalanced(node));
+        node.right = new Node(30);
+        node.right.left = new Node(25);
+        node.right.right = new Node(40);
+
+        isBalanced(node);
     }
 
-    public static boolean isBalanced(Node node) {
-        if (checkHeight(node) == -1) {
-            return false;
-        }
-        return true;
+    public static void isBalanced(Node node) {
+        if (checkHeight(node) == -1) System.out.println("Not balanced.");
+        else System.out.println("Balanced.");
     }
 
     public static int checkHeight(Node node) {
-        // Node has a height of zero
         if (node == null) return 0;
 
-        // Check if the left is balanced
         int leftHeight = checkHeight(node.left);
         if (leftHeight == -1) return -1;
 
-        // Check if the right is balanced
         int rightHeight = checkHeight(node.right);
         if (rightHeight == -1) return -1;
 
-        // Check if the curret node is balanced
         if (Math.abs(rightHeight - leftHeight) > 1) return -1;
 
         return Math.max(rightHeight, leftHeight) + 1;
